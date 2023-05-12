4ff9620ee4cbb6c24b888249fe82f53c355afcd3
Gustavo Ueti Fukunaga <gustavofukunaga@usp.br>
2023-05-12 17:42:16 -0300

    Update edit.js

--- edit.js
+++ edit.js
@@ -1,1 +1,25 @@
 
+//* add(first, second) /* adição de dois números */
+function add(first, second) {
+  return first + second;
+}
+
+//* sub(first, second) /* subtração de dois números */
+function sub(first, second) {
+  return first - second;
+}
+
+//* mul(first, second) /* multiplicação de dois números */
+function mul(first, second) {
+  return first * second;
+}
+
+//* div(first, second) /* divisão de dois números */
+function div(first, second) {
+  return first / second;
+}
+
+//* is_even(number)    /* verificação de que um número é par */
+function is_even(number) {
+  return number % 2 == 0;
+}
